e7e128a59bc6480328703c5c83afdacd3d7c9be0
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-12-07 20:19:04 +0900

    Add .gitingnoreファイル and コメントの追加

--- src/image-resize/main.go
+++ src/image-resize/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	// 元となるimageファイルを
+	// 元となるimageファイル
 	file, err := os.Open("./images/image01.jpg")
 	if err != nil {
 		log.Fatal(err)
@@ -21,6 +21,7 @@ func main() {
 	}
 	file.Close()
 
+	// 引数(幅, 高さ, 画像data, アルゴリズムの設定)
 	m := resize.Resize(1000, 0, img, resize.Lanczos3)
 
 	out, err := os.Create("./resized_images/r_image01.jpg")
